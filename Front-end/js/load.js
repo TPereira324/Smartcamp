1b38cd07f181718e38986f7b32fa6dfec1721cec
TPereira324 <tahawurpereira1@gmail.com>
2026-03-26 10:02:49 +0000

    colocando a framework do bootstrap

--- Front-end/js/load.js
+++ Front-end/js/load.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const view = new UtilizadorView();
     const controller = new UtilizadorController(model, view);
 
+    function setViewportHeight() {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    setViewportHeight();
+    window.addEventListener('resize', setViewportHeight);
+
     controller.init();
     console.log('Aplicação CocoRoot carregada!');
 });
